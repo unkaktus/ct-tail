cdaa07c61c1c0b1ceffc4a68a6aa5248d7c14284
Ivan Markin <sw@nogoegst.net>
2020-11-22 02:07:27 +0300

    Implement CT Tail webpage with long polling

--- cmd/ct-tail/main.go
+++ cmd/ct-tail/main.go
@@ -5,6 +5,9 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
+	"sync"
+	"sync/atomic"
 	"time"
 
 	cttail "github.com/nogoegst/ct-tail"
@@ -15,27 +18,95 @@ func currentOak() string {
 	return fmt.Sprintf("https://oak.ct.letsencrypt.org/%d/", year)
 }
 
+type LockableEntries struct {
+	Entries []*cttail.Entry
+	sync.RWMutex
+}
+
 func run() error {
 	var u = flag.String("u", "le-oak-current", "URL of the CT log")
 	var bygone = flag.Uint64("n", 0, "Number of entries before the current STH to fetch (max. 254)")
 	var interval = flag.Duration("i", 1*time.Second, "Interval between the fetches")
+	var webPort = flag.String("web-port", "", "Run CT Wall website on specified port")
 	flag.Parse()
+
 	if *u == "le-oak-current" {
 		*u = currentOak()
 	}
 	tailer := cttail.NewTailer(*u, *bygone)
-	for {
-		entries, err := tailer.FetchTip()
-		if err != nil {
-			return err
-		}
-		for _, entry := range entries {
-			jsonEntry, _ := json.Marshal(entry)
-			fmt.Printf("%s\n", jsonEntry)
-		}
-		time.Sleep(*interval)
-	}
 
+	var entries = LockableEntries{}
+	activeListeners := new(uint64)
+
+	go func() {
+		for {
+			if atomic.LoadUint64(activeListeners) != 0 {
+				log.Printf("%v active listeners, fetching", atomic.LoadUint64(activeListeners))
+				newEntries, err := tailer.FetchTip()
+				if err != nil {
+					log.Printf("fetching new entries: %v", err)
+					return
+				}
+				if len(newEntries) != 0 {
+					log.Printf("fetched %v entries", len(newEntries))
+				}
+				entries.Lock()
+				entries.Entries = newEntries
+				entries.Unlock()
+			}
+			time.Sleep(*interval)
+		}
+	}()
+
+	switch {
+	case *webPort != "":
+		h := http.NewServeMux()
+		h.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "text/html; charset=utf-8")
+			w.Write([]byte(staticPage))
+		}))
+		h.Handle("/log", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			atomic.AddUint64(activeListeners, 1)
+			defer atomic.AddUint64(activeListeners, ^uint64(0))
+
+			flusher := w.(http.Flusher)
+			w.Header().Set("X-Content-Type-Options", "nosniff")
+
+			for {
+				entries.RLock()
+				es := entries.Entries
+				entries.RUnlock()
+				for _, entry := range es {
+					jsonEntry, _ := json.Marshal(entry)
+					_, err := fmt.Fprintf(w, "%s\n", jsonEntry)
+					if err != nil {
+						log.Printf("cannot write to client: %v", err)
+						return
+					}
+				}
+				if len(entries.Entries) != 0 {
+					flusher.Flush()
+				}
+				time.Sleep(*interval)
+			}
+		}))
+		if err := http.ListenAndServe(":"+*webPort, h); err != nil {
+			return fmt.Errorf("serve HTTP: %v", err)
+		}
+	default:
+		atomic.AddUint64(activeListeners, 1)
+		for {
+			entries.RLock()
+			es := entries.Entries
+			entries.RUnlock()
+
+			for _, entry := range es {
+				jsonEntry, _ := json.Marshal(entry)
+				fmt.Printf("%s\n", jsonEntry)
+			}
+			time.Sleep(*interval)
+		}
+	}
 	return nil
 }
 
---
+++ cmd/ct-tail/staticpage.go
@@ -0,0 +1,62 @@
+package main
+
+var staticPage = `<html>
+<head>
+	<script>
+        function printLine(line) {
+            if (line == "") {
+                return
+            }
+            today = new Date()
+            date = today.toLocaleString()
+            line = date + "\t" + line
+
+            let elem = document.getElementById('log')
+            let lineElem = document.createElement('pre');
+            lineElem.append(line);
+            elem.append(lineElem);
+
+            lineElem.scrollIntoView()
+        }
+
+        function showEntry(entry) {
+            let line = ""
+            for (let name of entry.identifiers.dns_names) {
+                line += name
+            }
+            printLine(line)
+        }
+
+		var url = "log"
+		fetch(url).then(function (response) {
+		printLine("CT Log tailing started. DNS names for new log entries will appear below.")
+
+		let reader = response.body.getReader();
+		let decoder = new TextDecoder();
+		return readData();
+		function readData() {
+			return reader.read().then(function ({value, done}) {
+				let newData = decoder.decode(value, {stream: !done});
+				jsons = newData.split("\n")
+				for (let json of jsons) {
+					if (json != "") {
+						entry = JSON.parse(json)
+                        showEntry(entry)
+					}
+				}
+				if (done) {
+                    printLine("Stream ended.")
+					return;
+				}
+				return readData();
+			});
+		}
+	});
+	</script>
+</head>
+<body>
+    <div id="log">
+    </div>
+</body>
+</html>
+`
