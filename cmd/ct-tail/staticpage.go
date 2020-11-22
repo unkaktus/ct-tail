3582e7cbdb796b0056c940699182a8047ba90ff7
Ivan Markin <sw@nogoegst.net>
2020-11-22 03:01:54 +0300

    Start log only after the body is up

--- cmd/ct-tail/staticpage.go
+++ cmd/ct-tail/staticpage.go
@@ -27,31 +27,33 @@ var staticPage = `<html>
             printLine(line)
         }
 
-		var url = "log"
-		fetch(url).then(function (response) {
-		printLine("CT Log tailing started. DNS names for new log entries will appear below.")
+		window.onload = function runLog() {
+            printLine("CT Log tailing started. DNS names for new log entries will appear below.")
 
-		let reader = response.body.getReader();
-		let decoder = new TextDecoder();
-		return readData();
-		function readData() {
-			return reader.read().then(function ({value, done}) {
-				let newData = decoder.decode(value, {stream: !done});
-				jsons = newData.split("\n")
-				for (let json of jsons) {
-					if (json != "") {
-						entry = JSON.parse(json)
-                        showEntry(entry)
-					}
-				}
-				if (done) {
-                    printLine("Stream ended.")
-					return;
-				}
-				return readData();
-			});
-		}
-	});
+            var url = "log"
+            fetch(url).then(function (response) {
+                let reader = response.body.getReader();
+                let decoder = new TextDecoder();
+                return readData();
+                function readData() {
+                    return reader.read().then(function ({value, done}) {
+                        let newData = decoder.decode(value, {stream: !done});
+                        jsons = newData.split("\n")
+                        for (let json of jsons) {
+                            if (json != "") {
+                                entry = JSON.parse(json)
+                                showEntry(entry)
+                            }
+                        }
+                        if (done) {
+                            printLine("Stream ended.")
+                            return;
+                        }
+                        return readData();
+                    });
+                }
+            });
+        }
 	</script>
 </head>
 <body>
